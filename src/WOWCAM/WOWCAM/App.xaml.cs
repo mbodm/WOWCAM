c06035781f8cb52eeb5fac76132efb7ec0fdb8b1
MBODM <webmaster@mbodm.com>
2023-09-14 04:24:16 +0200

    Added .NET 4.8 WPF UI project, as SDK style project.

---
+++ src/WOWCAM/WOWCAM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WOWCAM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainWindow = new MainWindow(new Helper());
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow.Show();
+        }
+    }
+}
---
+++ src/WOWCAM/WOWCAM/IHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Documents;
+
+namespace WOWCAM
+{
+    public interface IHelper
+    {
+        string GetApplicationVersion();
+        void DisableHyperlinkHoverEffect(Hyperlink hyperlink);
+    }
+}

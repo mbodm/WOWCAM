0fff14ee9863917a1bbb97f88abe671e8afec6b7
MBODM <webmaster@mbodm.com>
2023-11-05 19:08:09 +0100

    Started adding Core stuff. Working on ConfigReader at the moment...

---
+++ src/WOWCAM/WOWCAM.Core/IConfigReader.cs
@@ -0,0 +1,16 @@
+﻿namespace WOWCAM.Core
+{
+    public interface IConfigReader
+    {
+        string Storage { get; } // Using such a generic term here, since config could be a file, or database, or whatever.
+
+        OperatingMode OperatingMode { get; }
+        IEnumerable<string> AddonUrls { get; }
+        string DownloadFolder { get; }
+        string UnzipFolder { get; }
+        string TempFolder { get; }
+
+        void ReadConfig();
+        void ValidateConfig(bool downloadMode, bool unzipMode);
+    }
+}
---
+++ src/WOWCAM/WOWCAM.Core/ICurseHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace WOWCAM.Core
+{
+    public interface ICurseHelper
+    {
+        string DisableScrollbarScript { get; }
+        string HideCookiebarScript { get; }
+        string GrabJsonScript { get; }
+
+        bool IsAddonPageUrl(string url);
+        bool IsFetchedDownloadUrl(string url);
+        bool IsRedirectWithApiKeyUrl(string url);
+        bool IsRealDownloadUrl(string url);
+
+        string GetAddonSlugNameFromAddonPageUrl(string url);
+        CurseHelperJson SerializeAddonPageJson(string json);
+        string BuildFetchedDownloadUrl(ulong projectId, ulong fileId);
+    }
+}

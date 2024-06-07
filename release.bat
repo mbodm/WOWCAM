6845e49bef483dc9f7d1da1b71eec0a7f9a1e7d0
MBODM <webmaster@mbodm.com>
2024-06-08 01:14:11 +0200

    dev...

--- release.bat
+++ release.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set CURRENT_FOLDER=%cd%
+set RELEASE_FOLDER=%CURRENT_FOLDER%\release
 set PROJECT_FOLDER_1=%CURRENT_FOLDER%\src\WOWCAM\WOWCAM
 set PUBLISH_FOLDER_1=%PROJECT_FOLDER_1%\bin\Release\net8.0-windows\win-x64\publish
 set PROJECT_FOLDER_2=%CURRENT_FOLDER%\src\wcupdate\wcupdate
@@ -9,20 +10,20 @@ set PUBLISH_FOLDER_2=%PROJECT_FOLDER_2%\bin\Release\net8.0\win-x64\publish
 cls
 echo.
 echo WOWCAM release script 1.0.0 (by MBODM 05/2024)
-
 echo.
+if not exist %RELEASE_FOLDER% mkdir %RELEASE_FOLDER%
+
 cd %PROJECT_FOLDER_1%
 dotnet publish -c Release
-cd %CURRENT_FOLDER%
-copy /B /V /Y %PUBLISH_FOLDER_1%\WOWCAM.exe %CURRENT_FOLDER%
-
+copy /B /V /Y %PUBLISH_FOLDER_1%\WOWCAM.exe %RELEASE_FOLDER%
 echo.
+
 cd %PROJECT_FOLDER_2%
 dotnet publish -c Release
-cd %CURRENT_FOLDER%
-copy /B /V /Y %PUBLISH_FOLDER_2%\wcupdate.exe %CURRENT_FOLDER%
-
+copy /B /V /Y %PUBLISH_FOLDER_2%\wcupdate.exe %RELEASE_FOLDER%
 echo.
+
+cd %CURRENT_FOLDER%
 echo|set /p="Have a nice day."
 echo.
 timeout /T 5

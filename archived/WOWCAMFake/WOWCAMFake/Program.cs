f107fe561a306be01a3b08adc1308b7a4ca5ed19
MBODM <webmaster@mbodm.com>
2024-12-07 12:33:44 +0100

    Added smaller fake .exe to test update mechanism.

---
+++ archived/WOWCAMFake/WOWCAMFake/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, i'm not WOWCAM.");
+Console.WriteLine();
+Console.WriteLine("- I'm just fake");
+Console.WriteLine("- I'm used to test WOWCAM's update mechanism");
+Console.WriteLine("- I'm way smaller than the real WOWCAM.exe");
+Console.WriteLine("- Therefore the GitHub test downloads are faster");
+Console.WriteLine();
+Console.Write("Press any key to exit...");
+Console.Read();
